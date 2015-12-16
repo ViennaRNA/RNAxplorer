ddc4ef98f9b2bb5e14d09b92d9e30f834b7cd516
Ronny Lorenz <ronny@tbi.univie.ac.at>
2015-12-16 14:03:13 +0100

    Changed mainloop in scripts/generateSamples.py

--- scripts/generateSamples.py
+++ scripts/generateSamples.py
@@ -80,9 +80,10 @@ def addToClusters(clusters, structures):
 
 def getInterestingClusters(clusters):
     """
-    Extract a list of interesting clusters that
-    we want to emphasize in the next sampling
-    iteration
+    Extract a list of interesting structures that
+    represent the provided clusters. These structures
+    are then used in the next sampling iteration to
+    emphasize a particular region in the landscape.
 
     This could be a cluster with low sample number,
     or one that has the highest distance to all the
@@ -90,40 +91,101 @@ def getInterestingClusters(clusters):
     """
 
 
-def generateSamples(seq, mfe_struct, mfe, iterations=0):
+def get2DBasins(samples, ref1, ref2):
     """
-    Generate a diverse structure sample set
-    by iteratively calling RNAxplorer
+    Given two reference structures ref1, and ref2, generate
+    the 2D projection of the provided sample set of structures.
+    Using the MFE representatives of each distance class,
+    determine the basins of attraction in this projection,
+    and associate each structure to on such basin.
+    Basin detection might be implemented using a flodding
+    technique that stops as soon as a saddle point in the
+    projection is encountered
+    """
 
-    In each iteration, we select a number of interesting
-    clusters, and take its representative structure as
-    input for RNAxplorer, to sample more structures from
-    the vicinity of this cluster. As second reference, we
-    take the MFE structure for now. This procedure may,
-    and actually should, be refined, since we expect too
-    many structures arround the MFE using this approach.
+
+
+def generateSamples(seq, mfe_struct, reference_stack):
     """
+    Generate a diverse structure sample set by calling
+    RNAxplorer for each pair of reference structures
+    that is supplied
+
+    After calling RNAxplorer we extract 2D basins from
+    the projection and retrieve a set of new interesting
+    reference structures from them.
+    """
+
+    # The global storage of all structures sampled in this run.
+    sample_set = []
+
+    # stack to hold interesting new reference structures
+    new_reference_stack = []
+
+
+    # now process all the structures in the reference_stack
+    # to obtain more structure samples. 
+    while len(reference_stack) > 0:
+        # at this point, pop two structures at a time to get
+        # new references and only default to using mfe_struct
+        # as first reference if we have just a single new_ref left
+        new_ref1 = reference_stack.pop()
+        if len(reference_stack) > 0:
+            new_ref2 = reference_stack.pop()
+        else
+            new_ref2 = mfe_struct
+
+        # call RNAxplorer with our reference structures
+        new_samples = callRNAxplorer(seq, new_ref1, new_ref2)
+
+        # determine which structures belong to the same region
+        # of interest, according to current 2D projection.
+        basins_2D = get2DBasins(samples, new_ref1, new_ref2)
+
+        # cluster the structures within each basin to
+        # obtain potentially interesting new reference
+        # structures
+        for b in basins_2D:
+            basin_clusters = getClusters(b)
+            s = getInterestingClusters(basin_clusters)
+            for c in s:
+                new_reference_stack.append(c)
+
+        # finally, we add the new_samples to our sample_set
+        for s in new_samples:
+            sample_set.append(s)
+
+    return sample_set, new_reference_stack
+
+
+def maintloop(seq, mfe_struct, mfe, iterations=0):
+    """
+    This is the mainloop that iteratively calls
+    generateSamples() to grow the global_structures list
+    """
+    # the global storage of all structures sampled so far.
+    # this variable is called clusters, however, we actually
+    # don't require the structures to be clustered here.
+    global_structures = []
 
     # Initial step:
     # start with mfe_struct and open chain as references
     # to retrieve first sample set
+    ref_structs = []
     openchain = "." * len(seq)
-    samples = callRNAxplorer(seq, mfe_struct, openchain)
+    ref_structs.append(mfe_struct)
+    ref_structs.append(openchain)
 
-    # add new clusters from current structure sample set
-    clusters = []
-    clusters = addToClusters(clusters, samples)
+    while iterations >= 0 and len(ref_structs) > 0:
+        # get new samples and potentially new reference structures
+        samples, new_ref_structs = generateSamples(seq, mfe_struct, ref_structs)
+        # add new samples to set of global structues
+        global_structures = addToClusters(global_structures, samples)
+        # assign new reference structures for next iteration
+        ref_structs = new_ref_structs
+        iterations = iteration - 1
 
-    # here comes the iterative step
-    for i in range(iterations):
-        interesting_clusters = getInterestingClusters(clusters)
-        for representative in intersting_clusters:
-            # create more samples by making mfe_struct and representative
-            # equally probable
-            new_samples = callRNAxplorer(seq, mfe_struct, representative)
-            clusters = addToClusters(clusters, new_samples)
-
-    return clusters
+    return global_structures
 
 
 if __name__ == "__main__":
