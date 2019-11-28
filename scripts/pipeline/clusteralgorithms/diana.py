4adf71cfea267835fe55d58b65343a1c07e90473
Gregor Entzian <g.entzian@viennarna.at>
2019-11-28 16:44:33 +0100

    + CH threshold

--- scripts/pipeline/clusteralgorithms/diana.py
+++ scripts/pipeline/clusteralgorithms/diana.py
@@ -85,6 +85,32 @@ def WmAll(c, dissMatrix):
             distanceSum += math.pow(dissMatrix[c[i]][c[j]], 2)
     return distanceSum
 
+def sum_distance_in_cluster(c, dissMatrix):
+    sum_dist = 0.0
+    for i in range(0, len(c)):
+        for j in range(i + 1, len(c)):
+            sum_dist += abs(dissMatrix[c[i]][c[j]])
+    return sum_dist
+
+def mean_distance_in_cluster(c, dissMatrix):
+    mean_dist = sum_distance_in_cluster(c, dissMatrix)
+    elements = ((len(c) * (len(c) -1)) / 2.0)
+    if elements <= 0:
+        return mean_dist
+    else:
+        mean_dist = mean_dist / elements
+    return mean_dist
+
+def mean_distance_in_matrix(dissMatrix, matrix_size):
+    avg_dist = 0
+    for i in range(matrix_size):
+        for j in range(i+1, matrix_size):
+            avg_dist += abs(dissMatrix[i][j])
+    n_dist = (float(matrix_size) * float(matrix_size-1))/2.0
+    avg_dist = avg_dist / n_dist
+    return avg_dist
+    
+
 def varianceRatio(parentCluster, clusterA, clusterB, dissMatrix):
     """
     compute the variance of the old clusters and the new cluster.
@@ -101,6 +127,41 @@ def varianceRatio(parentCluster, clusterA, clusterB, dissMatrix):
     randomRatio = (pow(len(clusterA.structures), 2.) + pow(len(clusterB.structures), 2.)) / (pow(len(parentCluster.structures), 2.))
     
     return withinRatio, randomRatio
+
+def pooled_within_group_sum_of_squares(clusters, dissMatrix):
+    pwgss = 0
+    if clusters == None:
+        return pwgss
+    for c in clusters:
+        pwgss += sum_distance_in_cluster(c, dissMatrix) / float(len(c))
+        """
+        faster alternatice is could be sum_i_N(pow(bpdist(centroid_structure(c), structure(c_i)),2))
+        """
+    return pwgss
+
+def pooled_between_group_sum_of_squares(clusters, dissMatrix, matrix_size):
+    pbgss = 0
+    if clusters == None:
+        return pbgss
+    avg_dist = mean_distance_in_matrix(dissMatrix, matrix_size)
+            
+    for c in clusters:
+        avg_dist_c = mean_distance_in_cluster(c, dissMatrix)
+        pbgss +=  abs(avg_dist_c - avg_dist) * float(len(c))
+        """
+        faster alternatice is could be sum_i_N(pow(bpdist(centroid_structure(dataset), centroid_structure(c_i)),2)) /float(len(c))
+        """
+    return pbgss
+
+def calinski_harabasz_index(clusters, dissMatrix, matrix_size):
+    ch = 0
+    pbgss = pooled_between_group_sum_of_squares(clusters, dissMatrix, matrix_size)
+    pwgss = pooled_within_group_sum_of_squares(clusters, dissMatrix)
+    number_of_clusters = len(clusters)
+    number_of_structures = matrix_size
+    ch =  ((pbgss) / (pwgss)) * ((number_of_structures - number_of_clusters) / float(number_of_clusters -1))
+    return ch
+    
 
 def maxDiameterInLeafNodes(clusterTree, dissMatrix):
     maxDiameter = -1
@@ -138,23 +199,25 @@ def averageDiameterInLeafNodes(clusterTree, dissMatrix):
     averageDiameter = sumDiameters / float(numberOfClusters)
     return averageDiameter
 
-def createClusterTree(c_root, dissMatrix, maxDiameterThreshold, maxAverageDiameterThreshold):
+def createClusterTree(c_root, dissMatrix, maxDiameterThreshold, maxAverageDiameterThreshold, structs, do_ch_first_local_min = False, hierarchy=1):
     """
     The core of the diana algorithm (recursive function).
     c_root = the rootnode of the clusterTree. It contains the main cluster as childnode.
     """
     # 1. select cluster with the largest diameter from all leafnodes.
-    maxDiameter, c_m = maxDiameterInLeafNodes(c_root, dissMatrix)
     dc = averageDiameterInLeafNodes(c_root, dissMatrix)
     if dc < maxAverageDiameterThreshold:
         return
     if maxDiameterThreshold >= 0:
+        maxDiameter, c_m = maxDiameterInLeafNodes(c_root, dissMatrix)
         if maxDiameter <= maxDiameterThreshold:
             return      
             
     if c_m == None:
         return
+    
     if len(c_m.structures) > 1:
+        hierarchy+=1
         # 2. object with highest dissimilarity to all others defines the new cluster (c_newA).
         o = objectIndexWithHighestDissimilarity(c_m.structures, dissMatrix)
         c_newA = Cluster()
@@ -186,24 +249,47 @@ def createClusterTree(c_root, dissMatrix, maxDiameterThreshold, maxAverageDiamet
         # if(criterion):
         c_m.childNodes.append(c_newB)
         c_m.childNodes.append(c_newA)
-            
+
+        if do_ch_first_local_min != False:
+            if do_ch_first_local_min == True:
+                do_ch_first_local_min = sys.float_info.max
+            cis = convertTreeToListOfClusters_indices(c_root, structs, [])
+            #print(cis, len(cis))
+            if len(cis) > 1:
+                ch = calinski_harabasz_index(cis, dissMatrix, len(structs))
+                if ch > do_ch_first_local_min:
+                    if len(c_m.childNodes) > 0:
+                        c_m.childNodes = []
+                        print(hierarchy, "ch", "{:10.3f}".format(ch))
+                    return # ch index > last ch index --> first local min --> break
+                print(hierarchy, "ch", "{:10.3f}".format(ch))
+                do_ch_first_local_min = ch
+
         # build the subtrees for each child
-        createClusterTree(c_root, dissMatrix, maxDiameterThreshold, maxAverageDiameterThreshold)
+        createClusterTree(c_root, dissMatrix, maxDiameterThreshold, maxAverageDiameterThreshold, structs, do_ch_first_local_min, hierarchy)
 
 
 def convertTreeToListOfClusters(clusterTree, structs, clusterList=[]):
-        if len(clusterTree.childNodes) > 0:
-            for cn in clusterTree.childNodes:
-                convertTreeToListOfClusters(cn, structs, clusterList)
-            return clusterList
-        else:
-            cluster = [ structs[c] for c in clusterTree.structures]
-            clusterList.append(cluster)
-    
+    if len(clusterTree.childNodes) > 0:
+        for cn in clusterTree.childNodes:
+            convertTreeToListOfClusters(cn, structs, clusterList)
+    else:
+        cluster = [ structs[c] for c in clusterTree.structures]
+        clusterList.append(cluster)
+    return clusterList
+
+def convertTreeToListOfClusters_indices(clusterTree, structs, clusterList=[]):
+    if len(clusterTree.childNodes) > 0:
+        for cn in clusterTree.childNodes:
+            convertTreeToListOfClusters_indices(cn, structs, clusterList)
+    else:
+        cluster = [ c for c in clusterTree.structures]
+        clusterList.append(cluster)
+    return clusterList 
          
 class DIANA:
     @staticmethod
-    def doClustering(structs, maxDiameter, maxAverageDiameter):
+    def doClustering(structs, maxDiameter, maxAverageDiameter, do_ch_first_local_min = False):
         """
         Computes the DIANA clustering
         
@@ -220,11 +306,17 @@ class DIANA:
         c_root.structures.extend([x for x in range(0, len(structs))])
         
         # start the real DIANA algorithm.
-        createClusterTree(c_root, dissMatrix, maxDiameter, maxAverageDiameter)
+        createClusterTree(c_root, dissMatrix, maxDiameter, maxAverageDiameter, structs, do_ch_first_local_min)
         # end DIANA
               
-        clusters = convertTreeToListOfClusters(c_root, structs)
+        clusters = convertTreeToListOfClusters(c_root, structs, [])
         
+        #cis = convertTreeToListOfClusters_indices(c_root, structs)
+        #if len(cis) > 1:
+        #    ch = calinski_harabasz_index(cis, dissMatrix, len(structs))
+        #    print("ch", ch)
+        #else:
+        #    print("ch", 0)
         return clusters
     
     @staticmethod
@@ -267,14 +359,16 @@ if __name__ == "__main__":
     parser.add_argument("-f", "--file", type=str, required=True, help="Fasta file with secondary structures")
     parser.add_argument("-d", "--diameter-threshold", type=int, default=0, required=False, help="Cluster diameter threshold (max bp distance within a cluster)")
     parser.add_argument("-m", "--average-diameter-threshold", type=float, default=0, required=False, help="Average diameter threshold")
+    parser.add_argument("-c", "--calinski-harabasz-threshold", action='store_true', default=False, required=False, help="Abort if the Calinski-Harabasz-Index reaches the first local minimum")
     args = parser.parse_args()
     structureFileName = args.file #sys.argv[1]
     structs = parseFile(structureFileName)
     maxDiameter = args.diameter_threshold
     maxAverageDiameter = args.average_diameter_threshold
     d = DIANA()
-    cluster = d.doClustering(structs, maxDiameter, maxAverageDiameter)
-    d.printClusters(cluster)
+    clusters = d.doClustering(structs, maxDiameter, maxAverageDiameter, args.calinski_harabasz_threshold)
+    d.printClusters(clusters)
+    
 
 
 
